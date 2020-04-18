1de5f3a986f2e579cee8b8ef433572578eace6c4
A Rohl <april.rohl2015@gmail.com>
2020-04-17 21:36:46 -0700

    npm added, more js code started, schema created

--- bamazon.sql
+++ bamazon.sql
@@ -1,2 +1,22 @@
+-- Drop db if one already exists, creates the new db --
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+-- Uses bamazon db and ensures all schemas attributed to it --
+USE bamazon_db;
+
+-- Creation of my products table --
+CREATE TABLE products (
+  item_id INT auto_increment NOT NULL,
+  product_name VARCHAR(75) NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  price DECIMAL(10,3) NOT NULL,
+  stock_quantity INT(10) NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+
+
+
 
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
 // Dependencies for required npm packages to run data in terminal
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var consoleTable = require("console.table");
+var table = require("console.table");
+var color = require("cli-color");
 
 // creating the connection to mysql
 var connection = connection.mysql({
@@ -14,6 +15,20 @@ var connection = connection.mysql({
 
 });
 
-// creating connection to the database
+// Creates the connection information for the sql database
 connection.connect(function(error){
-	if (error) throw error;
+    if (error) throw error;
+// Test console.log to check connection
+    console.log("connected as id " + connection.threadId + "\n");
+// run the function below after the connection is made to prompt the user to search store
+showProducts();
+});
+
+function showProducts() {
+    connection.query("SELECT * FROM products", function(error, res) {
+      if (error) throw error;
+      // Draw the table in the terminal using the response
+    console.table(res);
+    // Then prompt the customer for their choice of product, pass all the products to function promptCustomer
+    promptCustomer(res);
+  });

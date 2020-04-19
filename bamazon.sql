b93bc7915c1b7949804ffb8056e1cc3b5ab30657
A Rohl <april.rohl2015@gmail.com>
2020-04-19 12:05:23 -0700

    decimal update

--- bamazon.sql
+++ bamazon.sql
@@ -10,7 +10,7 @@ CREATE TABLE products (
   item_id INT auto_increment NOT NULL,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(50) NOT NULL,
-  price DECIMAL(10,3) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
   stock_quantity INT(10) NOT NULL,
   PRIMARY KEY (item_id)
 );
